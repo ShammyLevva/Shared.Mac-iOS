e27fd2f5a51229bff936b4e94824b6ec98cf318b
Alexander Bisset (Mac) <ambisset@btinternet.com>
2018-12-31 12:56:07 +0000

    Initial check-in of module Shared.Mac-iOS

---
+++ Properties/FileHandling.cs
@@ -0,0 +1,34 @@
+using System;
+using Foundation;
+
+#if __MACOS__
+using AppKit;
+#elif __IOS__
+using UIKit;
+#endif
+
+namespace FTAnalyzer.Properties
+{
+    [Register ("FileHandling")]
+#if __MACOS__
+    public class FileHandling : NSViewController
+#elif __IOS__
+    public class FileHandling : UIViewController
+#endif
+    {
+        public FileHandling(IntPtr handle) : base(handle) { }
+
+        public static FileHandling Default { get; } = (FileHandling)SettingsBase.Load(new FileHandling(new IntPtr()), typeof(FileHandling));
+
+        [DefaultSettingValue("False")]
+        public bool LoadWithFilters { get; set; }
+
+        [DefaultSettingValue("True")]
+        public bool RetryFailedLines { get; set; }
+
+        [DefaultSettingValue("True")]
+        public bool ConvertDiacritics { get; set; }
+
+        public void Save() => SettingsBase.Save(Default, typeof(FileHandling));
+    }
+}
---
+++ Properties/NonGedcomDate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Foundation;
+#if __MACOS__
+using AppKit;
+#elif __IOS__
+using UIKit;
+#endif
+
+namespace FTAnalyzer.Properties
+{
+    [Register("NonGedcomDate")]
+#if __MACOS__
+    public class NonGedcomDate : NSViewController
+#elif __IOS__
+    public class NonGedcomDate : UIViewController
+#endif
+    {
+        public NonGedcomDate(IntPtr handle) : base(handle) { }
+
+        public static NonGedcomDate Default { get; } = (NonGedcomDate)SettingsBase.Load(new NonGedcomDate(new IntPtr()), typeof(NonGedcomDate));
+
+        [DefaultSettingValue("False")]
+        public bool UseNonGedcomDates { get; set; }
+
+        [DefaultSettingValue(typeof(string), "dd/MM/yyyy")]
+        public string DateFormat { get; set; }
+
+        [DefaultSettingValue(typeof(string), "^\\d{1,2}\\/\\d{1,2}\\/\\d{4}$")]
+        public string Regex { get; set; }
+
+        [DefaultSettingValue(typeof(string), "/")]
+        public string Separator { get; set; }
+
+        [DefaultSettingValue(typeof(int), "1")]
+        public int FormatSelected { get; set; }
+
+        [DefaultSettingValue(typeof(string), "")]
+        public string Setting { get; set; }
+
+        public void Save() => SettingsBase.Save(Default, typeof(NonGedcomDate));
+    }
+}
